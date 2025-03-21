79756e04b2d840272324f5ca50d069f3a437096e
demid <demid090605@outlook.com>
2025-03-21 20:30:43 +0700

    Add: - Test output file - Function that outputs all files in folder which was given when program   execute 	изменено:      main.cpp 	новый файл:    test.tst

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
+using namespace filesystem;
 
 void parseArguments(int argc, char* argv[]);
 
+void scanDirectory(char* path);
 
 int main(int argc, char* argv[])
 {
 
      parseArguments(argc, argv);
+
      return 0;
 }
 
@@ -22,6 +26,24 @@ void parseArguments(int argc, char* argv[])
      
      for (int i = 0; i < argc; i ++ )
      {
+          cout << i << endl;
           cout << "Argument " << i << " - " << argv[i] << endl;
+          if (i == 1)
+          {
+               scanDirectory(argv[i]);
+          }
      }
 }
+
+
+// function that scans folder where image exist
+void scanDirectory(char* path)
+{
+     for (const auto& file : directory_iterator(path)) 
+     {
+          string FileName = file.path().string();
+          const char* cString = FileName.c_str();
+          cout << cString << endl;
+     }
+}
+
